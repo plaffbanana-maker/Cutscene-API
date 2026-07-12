f72e76ca3d8533f90295ef7ca9b8232dd3fe4c3b
plaffbanana-maker <plaffbanana@gmail.com>
2026-07-12 17:11:43 +0930

    Remove incompatible NeoForge event bus bridge

--- neoforge/src/main/java/net/thewinnt/cutscenes/neoforge/CutsceneAPINeoForge.java
+++ neoforge/src/main/java/net/thewinnt/cutscenes/neoforge/CutsceneAPINeoForge.java
@@ -3,10 +3,7 @@ package net.thewinnt.cutscenes.neoforge;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.NeoForge;
 import net.thewinnt.cutscenes.CutsceneAPI;
-import net.thewinnt.cutscenes.event.CutsceneEvents;
-import net.thewinnt.cutscenes.neoforge.event.CutsceneOverEvent;
 
 @Mod("cutscene_api")
 public final class CutsceneAPINeoForge {
@@ -20,8 +17,5 @@ public final class CutsceneAPINeoForge {
         if (dist == Dist.CLIENT) {
             CutsceneAPINeoForgeClient.init();
         }
-        CutsceneEvents.CUTSCENE_OVER_SERVER.addListener((type, id, player, reason) -> {
-            NeoForge.EVENT_BUS.post(new CutsceneOverEvent.Server(player, type, id, reason));
-        });
     }
 }
